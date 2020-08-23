7dcf459b616b770751886601c20ba95906277c8b
Hugo Castro <hugodecastro63@gmail.com>
2020-08-22 22:28:08 -0300

    inclusao metodos de controle

--- [SRP]-app_carrinho_compras/src/CarrinhoCompra.php
+++ [SRP]-app_carrinho_compras/src/CarrinhoCompra.php
@@ -3,7 +3,50 @@
 namespace App;
 
 class CarrinhoCompra {
-    public static function teste(){
-        return 'ok';
+    
+    private $itens;
+    private $status;
+    private $valorTotal;
+
+    public function __construct() {
+        $this->itens = [];
+        $this->status = 'aberto';
+        $this->valorTotal = 0;
     }
+
+    public function exibirItens() {
+        return $this->itens;
+    }
+
+    public function adicionarItem(string $item, float $valor) {
+        array_push($this->itens, ["item" => $item, "valor" => $valor]);
+        $this->valorTotal += $valor;
+        return true;
+    }
+
+    public function exibirValorTotal() {
+        return $this->valorTotal;
+    }
+
+    public function exibirStatus() {
+        return $this->status;
+    }
+
+    public function confirmarPedido() {
+        if($this->validarCarrinho()) {
+            $this->status = 'confirmado';
+            $this->enviarEmailConfirmacao();
+            return true;
+        }
+        return false;
+    }
+
+    public function enviarEmailConfirmacao() {
+        echo '<br />enviando e-mail de confirmação ...<br />';
+    }
+    
+    public function validarCarrinho() {
+        return count($this->itens) > 0;
+    }
+
 }
