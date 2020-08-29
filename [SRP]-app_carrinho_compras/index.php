10ec8a7c37a63daa21f5e338553a7ddc1aae9e6a
Hugo Castro <hugodecastro63@gmail.com>
2020-08-28 22:31:19 -0300

    removendo comentários antigos

--- [SRP]-app_carrinho_compras/index.php
+++ [SRP]-app_carrinho_compras/index.php
@@ -63,30 +63,3 @@ echo '<h4>E-mail</h4>';
 if($pedido->getStatus() == 'confirmado') {
    echo EmailService::dispararEmail();
 }
-
-// print_r($carrinho1->exibirItens());
-// echo 'Valor total: '.$carrinho1->exibirValorTotal();
-
-// /* $carrinho1->adicionarItem("Bicicleta", 750.10);
-// $carrinho1->adicionarItem("Geladeira", 1950.15);
-// $carrinho1->adicionarItem("Tapete", 350.20);
-//  */
-
-// $carrinho1->adicionarItem('Televisão 65"', 3570.25);
-
-// echo "<br />";
-// print_r($carrinho1->exibirItens());
-// echo 'Valor total: '.$carrinho1->exibirValorTotal();
-
-// echo "<br />";
-// echo 'status: '.$carrinho1->exibirStatus();
-
-// echo "<br />";
-// if($carrinho1->confirmarPedido()) {
-//     echo 'Pedido realizado com sucesso!';
-// } else {
-//     echo 'Erro na confirmação do pedido. Carrinho não possui itens';
-// }
-
-// echo "<br />";
-// echo 'status: '.$carrinho1->exibirStatus();
