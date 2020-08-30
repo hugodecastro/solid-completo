ea159318ed9bf1c7bb58f38b2a132fbbe12f057f
Hugo Castro <hugodecastro63@gmail.com>
2020-08-30 17:08:41 -0300

    leitor de arquivo csv

--- [OCP]-app_etl/index.php
+++ [OCP]-app_etl/index.php
@@ -2,4 +2,12 @@
 
 require __DIR__."/vendor/autoload.php";
 
-echo 'Funcionando';
+use src\Leitor;
+
+$leitor = new Leitor();
+$leitor->setDiretorio(__DIR__.'/files');
+$leitor->setArquivo('dados.csv');
+
+echo '<pre>';
+print_r($leitor->lerArquivo());
+echo '</pre>';
