06e3aa32940b3eb8bba56be667da28c6ba465ee5
Hugo Castro <hugodecastro63@gmail.com>
2020-09-13 16:06:20 -0300

    criando projeto app_poligonos

---
+++ [LSP]-app_poligonos/index.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+use src\Retangulo;
+use src\Quadrado;
+
+$retangulo = new Retangulo();
+$retangulo->setAltura(5);
+$retangulo->setLargura(10);
+
+echo '<h3>Área do retângulo: '.$retangulo->getArea().'</h3>';
+
+$quadrado = new Quadrado();
+$quadrado->setAltura(5);
+
+echo '<h3>Área do quadrado: '.$quadrado->getArea().'</h3>';
---
+++ [LSP]-app_poligonos/src/Quadrado.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace src;
+
+class Quadrado extends Retangulo {
+
+    public function setLargura(float $largura): void {
+        $this->largura = $largura;
+        $this->altura = $largura;
+    }
+
+    public function setAltura(float $altura): void {
+        $this->largura = $altura;
+        $this->altura = $altura;
+    }
+
+}
