03d7c4a407c85fd0bec9ccb916dc4dc23bca6fb7
Hugo Castro <hugodecastro63@gmail.com>
2020-09-18 21:30:25 -0300

    aplicando o principio ISP

--- [ISP]-app_crm/src/dao/ContratoModel.php
+++ [ISP]-app_crm/src/dao/ContratoModel.php
@@ -4,20 +4,10 @@ namespace src\dao;
 
 use src\BD;
 use src\interfaces\ICadastro;
-use src\componentes\Log;
-use src\componentes\Notificacao;
 
 class ContratoModel extends BD implements ICadastro {
 
     public function salvar() {
-        //TODO
-    }
-
-    public function registrarLog(Log $log) {
-        //TODO
-    }
-
-    public function enviarNotificacao(Notificacao $notificacao) {
         //TODO
     }
     
--- [ISP]-app_crm/src/dao/LeadModel.php
+++ [ISP]-app_crm/src/dao/LeadModel.php
@@ -4,16 +4,12 @@ namespace src\dao;
 
 use src\BD;
 use src\interfaces\ICadastro;
-use src\componentes\Log;
+use src\interfaces\INotificacao;
 use src\componentes\Notificacao;
 
-class LeadModel extends BD implements ICadastro {
+class LeadModel extends BD implements ICadastro, INotificacao {
 
     public function salvar() {
-        //TODO
-    }
-
-    public function registrarLog(Log $log) {
         //TODO
     }
 
