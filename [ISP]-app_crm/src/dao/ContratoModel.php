84598e217449c4ad0ed5bff961ae8fac7e551fb7
Hugo Castro <hugodecastro63@gmail.com>
2020-09-18 21:17:55 -0300

    implementando interface ICadastro

---
+++ [ISP]-app_crm/src/dao/ContratoModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace src\dao;
+
+use src\BD;
+use src\interfaces\ICadastro;
+use src\componentes\Log;
+use src\componentes\Notificacao;
+
+class ContratoModel extends BD implements ICadastro {
+
+    public function salvar() {
+        //TODO
+    }
+
+    public function registrarLog(Log $log) {
+        //TODO
+    }
+
+    public function enviarNotificacao(Notificacao $notificacao) {
+        //TODO
+    }
+    
+}
